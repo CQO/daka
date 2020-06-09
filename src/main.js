c03f5a0dea0b76d0b7ac6c62c11a7d40c50ca1de
puge <mail@puge.org>
2020-06-09 13:27:49 +0800

    获取房间数据

--- src/main.js
+++ src/main.js
@@ -6,3 +6,12 @@ function scrollIntoView () {
 // document.body.addEventListener('touchmove', function (e) {
 //   e.preventDefault() // 阻止默认的处理方式(阻止下拉滑动的效果)
 // }, {passive: false})
+
+// 数据库时间转时间
+function getTimeStr(num) {
+  let hour = parseInt(num / 3600)
+  let minute = num % 3600
+  if (hour < 10) hour = '0' + hour
+  if (minute < 10) minute = '0' + minute
+  return `${hour}:${minute}`
+}
